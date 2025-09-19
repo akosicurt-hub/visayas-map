422cbf4b6b9e0f184d3fb2825dfcef9e8e6b0631
akosicurt-hub <akosicurt@gmail.com>
2025-09-19 15:58:53 +0800

    Delete src directory

--- src/types/leaflet.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for Leaflet integration
-declare module 'leaflet' {
-  interface Icon {
-    Default: {
-      prototype: any;
-      mergeOptions(options: any): void;
-    };
-  }
-}
